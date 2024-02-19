0e308eebc0562b58bff754191ec10d5162b2e1d0
Amelia Sava <78620536+AmeliaSava@users.noreply.github.com>
2024-02-19 17:24:19 +0100

    Project Setup

---
+++ AudioApp/src/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace AudioApp 
+{
+	void Print();
+}
---
+++ AudioApp/src/Source.cpp
@@ -0,0 +1,15 @@
+#include "Header.h"
+#include <stdio.h>
+
+namespace AudioApp
+{
+	void Print()
+	{
+		printf("Toot\n");
+	}
+}
+
+void main()
+{
+	AudioApp::Print();
+}
